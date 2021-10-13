cd07a8b24c649c7f74e42d418b734d66a1ea026e
tobias <tobinoyacabj@gmail.com>
2021-10-13 17:56:09 -0300

    iniciado

--- main.c
+++ main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/// estructuras
+
+typedef struct nodoA
+{
+    int dato;
+    struct nodoA* izq;
+    struct nodoA* der;
+}nodoA;
+
+/// funciones
+
+void inicArbol (nodoA** arbol)
+{
+    *arbol = NULL;
+}
+
+nodoA* crearNodoA (int dato)
+{
+    nodoA* aux = (nodoA*)malloc(sizeof(nodoA));
+    aux->dato = dato;
+    aux->der = NULL;
+    aux->izq = NULL;
+    return aux;
+}
+
+
+
+
+/// main
+
 int main()
 {
+    nodoA* arbol;
+
+    inicArbol(&arbol);
+
+
+
 
     return 0;
 }
