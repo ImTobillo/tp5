52fd6d34e595ff02b967eff8ce675e47d86b536c
tobias <tobinoyacabj@gmail.com>
2021-10-14 01:35:59 -0300

    punto 1 hecho

--- main.c
+++ main.c
@@ -26,6 +26,42 @@ nodoA* crearNodoA (int dato)
     return aux;
 }
 
+int contarNodosHoja (nodoA* arbol)
+{
+    if (arbol)
+    {
+        if (arbol->izq == NULL && arbol->der == NULL)
+            return 1;
+        else
+            return contarNodosHoja(arbol->izq) + contarNodosHoja(arbol->der);
+    }
+    else
+        return 0;
+}
+
+int buscarDatoArbol (nodoA* arbol, int dato)
+{
+    int rta;
+
+    if (arbol)
+    {
+        if (arbol->dato == dato)
+            return 1;
+        else
+        {
+            rta = buscarDatoArbol(arbol->izq, dato);
+            if (rta == 0)
+            {
+                rta =
+            }
+        }
+
+    }
+    else
+        return 0;
+
+
+}
 
 
 
@@ -36,6 +72,19 @@ int main()
     nodoA* arbol;
 
     inicArbol(&arbol);
+
+    arbol = crearNodoA(3);
+    arbol->izq = crearNodoA(7);
+    arbol->izq->izq = crearNodoA(2);
+    arbol->der = crearNodoA(10);
+    arbol->der->izq = crearNodoA(1);
+    arbol->der->der = crearNodoA(12);
+    arbol->der->der->izq = crearNodoA(15);
+    arbol->der->der->der = crearNodoA(6);
+
+    //printf("%i", arbol->der->izq->dato);
+
+    printf("%i", contarNodosHoja(arbol));
 
 
 
